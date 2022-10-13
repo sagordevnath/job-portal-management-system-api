2f7cf904b4654d6163d8237c41a1432f88743f3a
sagor devnath <ssagor64@gmail.com>
2022-10-13 22:22:54 +0600

    create job by manager token job id

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -95,3 +95,67 @@ exports.getJobsByManagerToken = async (req, res) => {
   }
 };
 
+exports.getJobByManagerTokenJobId = async (req, res) => {
+  try {
+    const { email } = req.user;
+    //get user by this email from User model
+    const user = await User.findOne({ email }).select(
+      "-password -__v -createdAt -updatedAt -role -status -appliedJobs"
+    );
+    //get company by this user from Company model inside managerName field
+    const company = await Company.findOne({ managerName: user._id });
+
+    //get all jobs
+    const jobs = await Job.find({})
+      .populate({
+        path: "companyInfo",
+        select: "-jobPosts",
+      })
+      .populate({
+        path: "applications",
+        populate: {
+          path: "applicant",
+          select:
+            "-password -__v -createdAt -updatedAt -role -status -appliedJobs",
+        },
+        select: "-job",
+      })
+      .populate({
+        path: "companyInfo",
+        select: "-jobPosts",
+        populate: {
+          path: "managerName",
+          select:
+            "-password -__v -createdAt -updatedAt -role -status -appliedJobs",
+        },
+      });
+
+    //find the required job from jobs  with req.params id
+    const { id } = req.params;
+    const job = jobs.find((job) => {
+      return job._id.toString() == id.toString();
+    });
+
+    //check if managerName.email is equal to req.user.email
+    if (req.user.email !== job.companyInfo.managerName.email) {
+      return res.status(400).json({
+        status: "fail",
+        message: "You are not authorized to get internal data of this job",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        job,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "can't get the data",
+      error: error.message,
+    });
+  }
+};
+
